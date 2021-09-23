aeaf3d69165336f3fe17bc2b3019bc83fabf6e6f
cnettel <carl.nettelblad@it.uu.se>
2021-09-23 10:09:31 +0200

    CCE210

--- run_gcae.py
+++ run_gcae.py
@@ -461,8 +461,8 @@ def run_optimization(model, model2, optimizer, optimizer2, loss_function, input,
 		loss_value = tf.math.reduce_sum( -tf.reduce_sum((encoded_data-tf.roll(encoded_data, 1, axis=0))
 		* (encoded_data2-tf.roll(encoded_data2, 1, axis=0)), axis=-1)
 		/
-		tf.reduce_sum((encoded_data-tf.roll(encoded_data, 1, axis=0)) * (encoded_data-tf.roll(encoded_data, 1, axis=0)) + 
-		(encoded_data2-tf.roll(encoded_data2, 1, axis=0)) * (encoded_data2-tf.roll(encoded_data2, 1, axis=0))+1e-4, axis=-1))*1e-2
+		tf.stop_gradient(tf.reduce_sum((encoded_data-tf.roll(encoded_data, 1, axis=0)) * (encoded_data-tf.roll(encoded_data, 1, axis=0)) + 
+		(encoded_data2-tf.roll(encoded_data2, 1, axis=0)) * (encoded_data2-tf.roll(encoded_data2, 1, axis=0))+1e-4, axis=-1)))*1e-2
 	gradients2 = g2.gradient(loss_value, allvars)
 	other_loss = loss_value
 
@@ -472,8 +472,8 @@ def run_optimization(model, model2, optimizer, optimizer2, loss_function, input,
 		loss_value = tf.math.reduce_sum( tf.reduce_sum((encoded_data-tf.roll(encoded_data, 1, axis=0))
 		* (encoded_data2-tf.roll(encoded_data2, 2, axis=0)), axis=-1)
 		/
-		tf.reduce_sum((encoded_data-tf.roll(encoded_data, 1, axis=0)) * (encoded_data-tf.roll(encoded_data, 1, axis=0)) + 
-		(encoded_data2-tf.roll(encoded_data2, 2, axis=0)) * (encoded_data2-tf.roll(encoded_data2, 2, axis=0))+1e-4, axis=-1))*1e-2
+		tf.stop_gradient(tf.reduce_sum((encoded_data-tf.roll(encoded_data, 1, axis=0)) * (encoded_data-tf.roll(encoded_data, 1, axis=0)) + 
+		(encoded_data2-tf.roll(encoded_data2, 2, axis=0)) * (encoded_data2-tf.roll(encoded_data2, 2, axis=0))+1e-4, axis=-1)))*1e-2
 	##	output, encoded_data = model(input, targets, is_training=True)
 	##	#loss_value = loss_function(y_pred = output, y_true = targets) * (1.0 if pure or full_loss else 0.0)
 		#loss_value += 1e-3*tf.reduce_sum(tf.where(poplist[:, 0] == "AD_066", tf.math.reduce_sum(tf.square(encoded_data), axis=-1), 0.))
