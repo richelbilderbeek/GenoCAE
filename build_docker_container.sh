bc37b4c946564cc4af1a00bab7adfd337b17f0e3
richelbilderbeek <richel@richelbilderbeek.nl>
2022-02-10 21:06:42 +0100

    Can build Docker container

--- build_docker_container.sh
+++ build_docker_container.sh
@@ -8,7 +8,9 @@
 #
 #
 
-sudo docker container create -t gcae/genocae:build -f docker/build.dockerfile gcae --name gcae .
+# sudo docker container create -t gcae/genocae:build -f docker/build.dockerfile gcae --name gcae .
+# sudo docker run --name gcae/genocae:build -p 80:80 -d ubuntu:Apache_Server
+# sudo docker run --name gcae -p 80:80 -d gcae/genocae:build
 
 if [[ $HOSTNAME == "N141CU" ]]; then
   notify-send "Done creating Dockerfile" "Done creating Dockerfile"
