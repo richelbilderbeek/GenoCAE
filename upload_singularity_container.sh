ef567372a6ca2edc9bcb18b8699bbe0c6eebc1ed
richelbilderbeek <richel@richelbilderbeek.nl>
2022-03-28 17:30:37 +0200

    EOD

---
+++ upload_singularity_container.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Upload the Singularity container
+
+singularity remote use SylabsCloud
+
+# Log in, prevents error:
+#
+# FATAL:   Unable to push image to library: request did not succeed: Unauthorized (401 Unauthorized)
+#
+#
+echo "To login, paste '~/.singularity/sylabs-token'"
+echo 'Note that a token is valid for only 30 days, i.e. until february 14th 2022'
+echo 'singularity remote login'
+
+# Sign container
+echo "To sign the container, paste your passphrase"
+singularity sign gcae.sif
+
+# Upload the latest version
+singularity push gcae.sif library://richelbilderbeek/default/gcae:0.3
+
+if [[ $HOSTNAME == "N141CU" ]]; then
+  notify-send "Done uploading 'gcae.sif'" "Done uploading 'gcae.sif'"
+fi
+
